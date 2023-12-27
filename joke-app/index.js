0ae59ec465bb1ee88368c53bef8700cbacac8529
lawass1864 <amwass4@hotmail.com>
2023-12-27 11:59:59 +0100

    generateur de blague ok

--- joke-app/index.js
+++ joke-app/index.js
@@ -5,21 +5,32 @@
 const header = document.getElementById("header");
 const content = document.getElementById("content");
 
-
-/* Le code effectue une requête GET à l'URL "https://api.blablagues.net/?rub=blagues" à l'aide de la
+function getJoke() {
+  /* Le code effectue une requête GET à l'URL "https://api.blablagues.net/?rub=blagues" à l'aide de la
 fonction fetch. */
-fetch("https://api.blablagues.net/?rub=blagues")
-  /* Le code utilise la fonction « fetch » pour effectuer une requête GET à l'URL spécifiée. La partie
-  `.then((res) => res.json())` enchaîne une promesse qui convertit la réponse du serveur au format
-  JSON. La partie `.then((data) => {` est une autre promesse qui gère les données JSON renvoyées par
-  le serveur. */
-  .then((res) => res.json())
-  .then((data) => {
-    console.log(data.data.content);
-    header.textContent = data.data.content.text_head;
-    // Pour le text.hidden = devinette. On va faire une ternaire
-    content.textContent =
-      data.data.content.text !== ""
-        ? data.data.content.text
-        : data.data.content.text_hidden;
-  });
+  fetch("https://api.blablagues.net/?rub=blagues")
+    /* Le code utilise la fonction « fetch » pour effectuer une requête GET à l'URL spécifiée. La partie
+`.then((res) => res.json())` enchaîne une promesse qui convertit la réponse du serveur au format
+JSON. La partie `.then((data) => {` est une autre promesse qui gère les données JSON renvoyées par
+le serveur. */
+    .then((res) => res.json())
+    .then((data) => {
+   
+  /* Ce code extrait l'objet `content` de l'objet `data` renvoyé par l'API. Il enregistre ensuite
+  l'objet « content » sur la console. */
+      const { content } = data.data;
+      console.log(content);
+
+      header.textContent = content.text_head;
+      // Pour le text.hidden = devinette. On va faire une ternaire
+      content.textContent = content.text !== "" ? content.text : content.text_hidden;
+    });
+}
+
+getJoke();
+
+/* `document.body.addEventListener("click", getJoke);` is adding an event listener to the body element
+of the HTML document. It listens for a click event on the body, and when the click event occurs, it
+calls the `getJoke` function. This means that whenever the body is clicked, the `getJoke` function
+will be executed, fetching a new joke from the API and updating the content on the page. */
+document.body.addEventListener("click", getJoke);
